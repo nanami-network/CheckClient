02ac7f768bde3365401214e0dc8407e98c8c8947
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2022-06-21 18:24:31 +0900

    todo: add minecraft byte wrapper

--- src/main/java/xyz/n7mn/dev/checkclient/packet/PacketReceiveEvent.java
+++ src/main/java/xyz/n7mn/dev/checkclient/packet/PacketReceiveEvent.java
@@ -4,11 +4,19 @@ import com.lunarclient.bukkitapi.nethandler.client.LCPacketModSettings;
 import com.lunarclient.bukkitapi.nethandler.client.obj.ModSettings;
 import io.github.retrooper.packetevents.PacketEvents;
 import io.github.retrooper.packetevents.event.PacketListenerAbstract;
-import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
+import io.github.retrooper.packetevents.event.impl.*;
 import io.github.retrooper.packetevents.packettype.PacketType;
+import io.github.retrooper.packetevents.packetwrappers.WrappedPacket;
+import io.github.retrooper.packetevents.packetwrappers.handshaking.setprotocol.WrappedPacketHandshakingInSetProtocol;
+import io.github.retrooper.packetevents.packetwrappers.login.in.custompayload.WrappedPacketLoginInCustomPayload;
+import io.github.retrooper.packetevents.packetwrappers.login.out.custompayload.WrappedPacketLoginOutCustomPayload;
 import io.github.retrooper.packetevents.packetwrappers.play.in.custompayload.WrappedPacketInCustomPayload;
 import io.github.retrooper.packetevents.packetwrappers.play.out.custompayload.WrappedPacketOutCustomPayload;
+import io.github.retrooper.packetevents.utils.player.ClientVersion;
 import io.github.retrooper.packetevents.utils.server.ServerVersion;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import org.bukkit.Bukkit;
 import xyz.n7mn.dev.checkclient.CheckClientInstance;
 import xyz.n7mn.dev.checkclient.check.CheckMCBrandAbstract;
 import xyz.n7mn.dev.checkclient.data.PlayerData;
@@ -19,7 +27,10 @@ import xyz.n7mn.dev.checkclient.util.FeatherUtils;
 import xyz.n7mn.dev.checkclient.util.ForgeUtils;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PacketReceiveEvent extends PacketListenerAbstract {
 
@@ -80,6 +91,75 @@ public class PacketReceiveEvent extends PacketListenerAbstract {
             } catch (Exception ex) {
                 data.getClientData().setType(ClientType.MODIFIED_CLIENT);
             }
+        }
+    }
+
+    private Map<Object, ClientVersion> hashMap = new HashMap<>();
+
+    @Override
+    public void onPacketLoginReceive(PacketLoginReceiveEvent event) {
+        try {
+            if (event.getPacketId() == PacketType.Login.Client.CUSTOM_PAYLOAD) {
+
+                WrappedPacketLoginInCustomPayload wrapper = new WrappedPacketLoginInCustomPayload(event.getNMSPacket());
+
+                if (wrapper.getMessageId() == 1212) {
+                    Bukkit.getLogger().info("wrapper:" + new String(wrapper.getData(), StandardCharsets.UTF_8));
+                }
+
+                Bukkit.getLogger().info("packet:" + "incoming!");
+
+            } else if (event.getPacketId() == PacketType.Login.Client.START) {
+                ClientVersion version = hashMap.get(event.getChannel());
+
+                if (version.isNewerThanOrEquals(ClientVersion.v_1_13) && PacketEvents.get().getServerUtils().getVersion().isNewerThanOrEquals(ServerVersion.v_1_13)) {
+                    if (version.isOlderThan(ClientVersion.v_1_17)) {
+                        PacketEvents.get().getPlayerUtils().sendPacket(event.getChannel(), new WrappedPacketLoginOutCustomPayload(1212, "fml:handshake", new byte[]{1, 0, 0, 0}));
+                    } else {
+                        //PacketEvents.get().getPlayerUtils().sendPacket(event.getChannel(), new WrappedPacketLoginOutCustomPayload(1212, "fml:handshake", new byte[]{2, 0, 0, 0}));
+                        //todo: v1_17 handshake
+                    }
+                }
+
+                hashMap.remove(event.getChannel());
+            }
+        } catch (Exception ex) {
+            //memory leak fix
+            hashMap.remove(event.getChannel());
+        }
+    }
+
+    @Override
+    public void onPacketHandshakeReceive(PacketHandshakeReceiveEvent event) {
+        WrappedPacketHandshakingInSetProtocol wrapper = new WrappedPacketHandshakingInSetProtocol(event.getNMSPacket());
+
+        final ClientVersion version = ClientVersion.getClientVersion(wrapper.getProtocolVersion());
+
+        hashMap.put(event.getChannel(), version);
+    }
+
+    @Override
+    public void onPacketPlaySend(PacketPlaySendEvent event) {
+        if (event.getPacketId() == PacketType.Login.Server.CUSTOM_PAYLOAD) {
+            WrappedPacketLoginOutCustomPayload wrapper = new WrappedPacketLoginOutCustomPayload(event.getNMSPacket());
+
+            Bukkit.getLogger().info("wrapper:" + Arrays.toString(wrapper.getData()));
+        }
+    }
+
+    @Override
+    public void onPostPacketPlayReceive(PostPacketPlayReceiveEvent event) {
+        if (event.getPacketId() == PacketType.Login.Server.CUSTOM_PAYLOAD) {
+            WrappedPacketLoginOutCustomPayload wrapper = new WrappedPacketLoginOutCustomPayload(event.getNMSPacket());
+
+            Bukkit.getLogger().info("wrapper:" + Arrays.toString(wrapper.getData()));
+        }
+    }
+
+    @Override
+    public void onPacketLoginSend(PacketLoginSendEvent event) {
+        if (event.getPacketId() == PacketType.Login.Server.DISCONNECT) {
+            hashMap.remove(event.getChannel());
         }
     }
 
